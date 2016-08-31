04e8d68f03d295819d29162a84ce122dbedfbc1e
cookm346 <cookm346@myumanitoba.ca>
2016-08-31 11:36:44 -0500

    removed Genus Binner example -- redundant and contained less info

--- GenusBinner/Code/BinExample.R
+++
@@ -1,8 +0,0 @@
-#set working directory
-    setwd("C:/Users/HIV-Proteomics/Desktop/Matt/BurgenerLab/GenusBinner/Code/ExampleData")
-
-#read in data    
-    data <- read.csv("ExampleData.csv", header = TRUE, row.names = 1)    
-
-#bin data by genus    
-    results <- binByGenus(data)
